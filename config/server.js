fa2b4c202c492d401bab7baa079bbe168688f8ba
Jules DAYAUX <julesdayaux@macbook-pro-de-jules.home>
2021-08-02 23:34:19 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dd793ed839486286126bb948a2ab8dca'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b507d706-a50e-4d12-b1df-ca9362012178'
+};
