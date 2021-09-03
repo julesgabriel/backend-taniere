a8c12317c70526ae4af58d83184be223229eec1e
Jules DAYAUX <julesdayaux@mbp-de-jules.home>
2021-09-03 16:30:42 +0200

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,3 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'dd793ed839486286126bb948a2ab8dca'),
-    },
-  },
+  url: env('MY_HEROKU_URL'),
 });
