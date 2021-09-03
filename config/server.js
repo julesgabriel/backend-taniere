9b3eed1c23d9039b3a40930c81658fb49f401b43
Jules DAYAUX <julesdayaux@mbp-de-jules.home>
2021-09-03 16:43:52 +0200

    UPDATE .env production config

--- config/server.js
+++ config/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
-  url: env('MY_HEROKU_URL'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dd793ed839486286126bb948a2ab8dca'),
+    },
+  },
 });
